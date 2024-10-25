0ee4517e22c9546cf78d79c1771568c793ea7ef2
Cathy Whitteker <56772806+desertkitten@users.noreply.github.com>
2024-10-25 20:27:13 +0000

    Edited HTML file. Starting javascript file

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+let playerScore = 0;
+let computerScore = 0;
+
+const playerScoreDisplay = document.getElementById('playerScore');
+const computerScoreDisplay = document.getElementById('computerScore');
+const resultDisplay = document.getElementById('result');
+const winnerDisplay = document.getElementById('winner');
+
+
+
+
+
+
 function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3);
     if (randomNumber === 0) {
